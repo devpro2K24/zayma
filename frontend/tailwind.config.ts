eea41eca1801fe03ccc777f53eab65a98d9575a8
Mouhamed Lo <160361321+m3dev4@users.noreply.github.com>
2024-12-20 03:12:14 +0000

    test frontend

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,17 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        sans: ['Robotto', 'sans-serif'],
+      },
+      fontSize:{
+        customSearch: '1.35rem'
+      },
+      padding: {
+        customPad: '.35em .35em 0'
+      }
     },
   },
   plugins: [],
